3505a7ef2addd8dc986be7204de34deb423ca429
Jeff Crocker <crockagile@gmail.com>
2022-10-19 16:06:48 -0400

    doc panic

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -2,6 +2,8 @@
 #![warn(missing_docs, clippy::all, clippy::pedantic, clippy::cargo)]
 
 /// WIP
+/// # Panics
+/// always panics because this is a work in progress
 pub fn init() {
     panic!("WIP")
 }
